bd94d64af99e3f1166decedf581ba4827dc630a9
aaronossei@gmail.com <aaronossei@gmail.com>
2024-03-10 13:01:11 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,3 +10,12 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def enable_search_box(self, **event_args):
+    """This method is called when the text in this text box is edited"""
+    self.search_button.enabled = self.location_name.text != "" and self.location_search.text != ""
+
+  def search_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    
+
